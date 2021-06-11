539564d6e2be27b904be500b0f58552543198f91
Macsik121 <hiomimipad@gmail.com>
2021-06-11 09:55:39 +0300

    ui deploy

--- uiserver.js
+++ uiserver.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.UI_SERVER_PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.use('/', express.static('public'));
 
